68b73abd4a1555298b16bc2c4d2959d015bc6957
JosepharDev <josephardev@gmail.com>
2025-09-21 21:12:12 +0100

    handle all signals

--- cpp/Daemon.cpp
+++ cpp/Daemon.cpp
@@ -91,12 +91,14 @@ void DaemonApp::signal_handler(int sig)
 bool DaemonApp::setup_signals()
 {
     instance_ = this;
-    if (signal(SIGINT, &DaemonApp::signal_handler) == SIG_ERR ||
-        signal(SIGTERM, &DaemonApp::signal_handler) == SIG_ERR ||
-        signal(SIGQUIT, &DaemonApp::signal_handler) == SIG_ERR)
-    {
-        return false;    
+
+    for (int sig = 1; sig < NSIG; ++sig) {
+        if (sig == SIGKILL || sig == SIGSTOP || sig == SIGCONT ) {
+            continue;
+        }
+        signal(sig, &DaemonApp::signal_handler);
     }
+
     return true;
 }
 
@@ -131,20 +133,47 @@ bool DaemonApp::init(){
     }
     return true;
 }
+#include <csignal>
+#include <string>
+
 std::string DaemonApp::signal_name(int sig)
 {
     switch (sig)
     {
-        case SIGINT:
-            return "SIGINT";
-        case SIGTERM:
-            return "SIGTERM";
-        case SIGQUIT:
-            return "SIGQUIT";
+        case SIGHUP:   return "SIGHUP";
+        case SIGINT:   return "SIGINT";
+        case SIGQUIT:  return "SIGQUIT";
+        case SIGILL:   return "SIGILL";
+        case SIGTRAP:  return "SIGTRAP";
+        case SIGABRT:  return "SIGABRT";
+        case SIGBUS:   return "SIGBUS";
+        case SIGFPE:   return "SIGFPE";
+        case SIGUSR1:  return "SIGUSR1";
+        case SIGSEGV:  return "SIGSEGV";
+        case SIGUSR2:  return "SIGUSR2";
+        case SIGPIPE:  return "SIGPIPE";
+        case SIGALRM:  return "SIGALRM";
+        case SIGTERM:  return "SIGTERM";
+        case SIGSTKFLT:return "SIGSTKFLT";
+        case SIGCHLD:  return "SIGCHLD";
+        case SIGCONT:  return "SIGCONT";
+        case SIGTSTP:  return "SIGTSTP";
+        case SIGTTIN:  return "SIGTTIN";
+        case SIGTTOU:  return "SIGTTOU";
+        case SIGURG:   return "SIGURG";
+        case SIGXCPU:  return "SIGXCPU";
+        case SIGXFSZ:  return "SIGXFSZ";
+        case SIGVTALRM:return "SIGVTALRM";
+        case SIGPROF:  return "SIGPROF";
+        case SIGWINCH: return "SIGWINCH";
+        case SIGPOLL:  return "SIGPOLL";
+        case SIGPWR:   return "SIGPWR";
+        case SIGSYS:   return "SIGSYS";
         default:
-            return std::to_string(sig);
+            return "UNKNOWN(" + std::to_string(sig) + ")";
     }
-};
+}
+
 
 int DaemonApp::run()
 {
